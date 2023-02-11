54dd6cb82339a8c2182f718b6d5cbadfb5998dd6
clingfei <1599101385@qq.com>
2023-02-11 14:38:54 +0800

    update lab2

--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -18,14 +18,18 @@ class TCPReceiver {
     StreamReassembler _reassembler;
 
     //! The maximum number of bytes we'll store.
-    size_t _capacity;
+    size_t _capacity = 0;
+    WrappingInt32 isn = WrappingInt32(0);
+    uint64_t _ackno = 0;
+    bool _syn = false;
+    bool _fin = false;
 
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+     TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
