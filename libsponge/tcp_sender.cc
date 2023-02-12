3e9329ed7395502f94ab6783af6a09a1fdbda368
clingfei <1599101385@qq.com>
2023-02-12 20:08:03 +0800

    finish lab3

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -20,19 +20,117 @@ using namespace std;
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
     : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
     , _initial_retransmission_timeout{retx_timeout}
+    , _retransmission_timeout{retx_timeout}
     , _stream(capacity) {}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
+uint64_t TCPSender::bytes_in_flight() const { return _bytes_in_flight; }
 
-void TCPSender::fill_window() {}
+bool TCPSender::send_segment(TCPSegment &segment, const size_t length) {
+    std::cout << "length: "<< length << std::endl;
+    if (_next_seqno == 0) 
+        segment.header().syn = true;
+    segment.header().seqno = wrap(_next_seqno, _isn);
+    if (length < size_t(segment.header().syn))
+        return false;
+    // MAX_PAYLOAD_SIZE limits payload only.
+    std::string payload = _stream.read(min(TCPConfig::MAX_PAYLOAD_SIZE, length - segment.header().syn));
+    segment.payload() = Buffer(std::move(payload));
+    if (!_fin && _stream.eof() && length > segment.length_in_sequence_space()) {
+        _fin = true;
+        segment.header().fin = true;
+    }
+    std::cout << "payload: " << segment.payload().str() << std::endl;
+    std::cout << segment.length_in_sequence_space() << _stream.eof() << std::endl;
+    if (segment.length_in_sequence_space() == 0)
+        return false;
+    else {
+        _next_seqno += segment.length_in_sequence_space();
+        _bytes_in_flight += segment.length_in_sequence_space();
+        _segments_out.push(segment);
+        _segments_unacknowledged.push(segment);
+        return !_stream.eof();
+    }
+}
+
+// 长度非0的segment发送后，如果timer未启动，启动timer
+void TCPSender::fill_window() {
+    // 发送窗口 = 已发送未确认 + 可发送但未发送
+    // 发送窗口由接收方的窗口大小决定
+    size_t fill_window = 0;
+    // if receiver announce a window size of zero, fill_window method should act like the window_size is one
+    if (_window_size == 0) {
+        if (_bytes_in_flight == 0) {
+            TCPSegment segment;
+            send_segment(segment, 1);
+        } else 
+            return;
+    } else {
+        while (true) {
+            TCPSegment segment;
+            if (_window_size > _bytes_in_flight) 
+                fill_window = _window_size - _bytes_in_flight;
+            else 
+                fill_window = 0;
+            std::cout << "fill_window: " << fill_window << std::endl;
+            if (!send_segment(segment, fill_window))
+                break;
+        }
+    }
+    if (!_segments_unacknowledged.empty()) 
+        _timer.enable(_retransmission_timeout);
+}
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { 
+    uint64_t _ackno = unwrap(ackno, _isn, _next_seqno);
+    _window_size = window_size;
+    std::cout << "_ackno: " << _ackno << " _next_seqno: " << _next_seqno << " _bytes_in_flight: " << _bytes_in_flight << std::endl;
+    if (_ackno > _next_seqno || _ackno <= _next_seqno - _bytes_in_flight) 
+        return;
+    bool reset = false;
+    // look through its collection of outstanding segments and remove any that have now been fully acknowledged
+    while (!_segments_unacknowledged.empty()) {
+        TCPSegment segment = _segments_unacknowledged.front();
+        std::cout << "lentgh in sequence space: " << segment.length_in_sequence_space() << std::endl;
+        std::cout << "unwrap: " << unwrap(segment.header().seqno, _isn, _next_seqno) << std::endl;
+        if (unwrap(segment.header().seqno, _isn, _next_seqno) + segment.length_in_sequence_space() <= _ackno) {
+            _bytes_in_flight -= segment.length_in_sequence_space();
+            std::cout << "bytes in flight: " << _bytes_in_flight << std::endl;
+            _segments_unacknowledged.pop();
+            reset = true;
+        } else 
+            break;
+    }
+    if (reset) {
+        _retransmission_timeout = _initial_retransmission_timeout;
+        _consecutive_retransmissions = 0;
+        if (!_segments_unacknowledged.empty()) 
+            _timer.reset(_retransmission_timeout);
+        else 
+            _timer.disable();
+    }
+ }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPSender::tick(const size_t ms_since_last_tick) { 
+    if (_segments_unacknowledged.empty()) 
+        return;
+    _timer.tick(ms_since_last_tick);
+    if (_timer.is_timeout()) {
+        _segments_out.push(_segments_unacknowledged.front());
+        if (_window_size > 0) {
+            _retransmission_timeout *= 2;
+            _consecutive_retransmissions++;
+        }
+        _timer.reset(_retransmission_timeout);
+    }
+ }
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+unsigned int TCPSender::consecutive_retransmissions() const { return _consecutive_retransmissions; }
 
-void TCPSender::send_empty_segment() {}
+void TCPSender::send_empty_segment() {
+    TCPSegment segment;
+    segment.header().seqno = wrap(_next_seqno, _isn);
+    _segments_out.push(segment);
+}
--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -11,6 +11,24 @@
 
 //! \brief The "sender" part of a TCP implementation.
 
+class Timer {
+private:
+  size_t _rto = 0;
+  size_t elapsed_time = 0;
+  bool running = false;
+
+public:
+  Timer() {}
+  void enable(size_t rto) { if (!running) { running = true; _rto = rto; } }
+  void disable() { running = false; elapsed_time = 0; }
+
+  void tick(const size_t ms_since_last_tick) { elapsed_time += ms_since_last_tick; }
+
+  bool is_timeout() { return elapsed_time >= _rto; }
+
+  void reset(size_t new_rto = 0) { running = true; _rto = new_rto; elapsed_time = 0; }
+};
+
 //! Accepts a ByteStream, divides it up into segments and sends the
 //! segments, keeps track of which segments are still in-flight,
 //! maintains the Retransmission Timer, and retransmits in-flight
@@ -22,15 +40,24 @@ class TCPSender {
 
     //! outbound queue of segments that the TCPSender wants sent
     std::queue<TCPSegment> _segments_out{};
+    std::queue<TCPSegment> _segments_unacknowledged{};
 
     //! retransmission timer for the connection
-    unsigned int _initial_retransmission_timeout;
+    size_t _initial_retransmission_timeout;
+    size_t _retransmission_timeout;
 
     //! outgoing stream of bytes that have not yet been sent
     ByteStream _stream;
 
     //! the (absolute) sequence number for the next byte to be sent
     uint64_t _next_seqno{0};
+
+    Timer _timer{};
+
+    size_t _window_size = 1;
+    size_t _bytes_in_flight = 0;
+    int _consecutive_retransmissions = 0;
+    bool _fin = false;
 
   public:
     //! Initialize a TCPSender
@@ -52,6 +79,9 @@ class TCPSender {
 
     //! \brief Generate an empty-payload segment (useful for creating empty ACK segments)
     void send_empty_segment();
+
+    //! \brief create segment for specified fill_window
+    bool send_segment(TCPSegment &segment, const size_t bytes_to_send);
 
     //! \brief create and send segments to fill as much of the window as possible
     void fill_window();
@@ -89,4 +119,24 @@ class TCPSender {
     //!@}
 };
 
+// 每个TCPSender对应不止一个ticker，每次调用tick，遍历set，调用elapsed，判断是否超时，如果超时则重发segment，
+
+// sender周期性调用tick方法判断有没有超时
+// 如果超时没有收到确认号，则应该重发报文
+// 1. 每隔几毫秒调用tick，参数代表距离上次调用的时间，也就代表了TCPSender保持活跃的时间
+// 2. 构造TCPSender时，指定参数RTO的初始值——代表距离下次重发的时间， 
+//        _initial_retransmission_timeout中保存初始值。RTO可变，但是初始值不变
+// 3. timer：RTO超时后alarm，对时间的计数来自于tick
+// 4. 每次长度非0的segment发送后，如果timer没有运行，则开始运行timer，
+// 5. 接受到对应的响应后，停止timer
+// 6. 如果调用了tick，并且retransmission timer超时，
+//          a) 重发最早的没有被完全确认的segment，
+//          b) 如果window size不为0
+//                跟踪the number of consecutive retrasmissions(指的是重发的报文数？)
+//                RTO加倍--快速退避，减少重传
+//          c) RTO超时后重置timer，
+// 7. 接收到新的更大的ackno时
+//             将RTO设置回初始值
+//             如果有outstanding data, 重置timer
+//             consecutive retrasmissions 设置为0;
 #endif  // SPONGE_LIBSPONGE_TCP_SENDER_HH
